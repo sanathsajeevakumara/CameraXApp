a871c08e47fe8a165c14d44631a2932da348f7f3
Sanath <sanath@entrusttitle.com>
2023-04-11 13:36:34 +0530

    handle required permission and Added camera values

---
+++ app/src/main/java/com/sanathcoding/cameraxapp/CameraValues.kt
@@ -0,0 +1,23 @@
+package com.sanathcoding.cameraxapp
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.content.ContextCompat
+
+object CameraValues {
+    private const val TAG = "cameraXApp"
+    private const val FILE_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    public val REQUIRED_PERMISSION =
+        mutableListOf(
+            android.Manifest.permission.CAMERA
+        ).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+
+    fun hasPermission(context: Context) = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
--- app/src/main/java/com/sanathcoding/cameraxapp/MainActivity.kt
+++ app/src/main/java/com/sanathcoding/cameraxapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.sanathcoding.cameraxapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageCapture
+import androidx.camera.view.LifecycleCameraController
+import com.sanathcoding.cameraxapp.CameraValues.REQUIRED_PERMISSION
+import com.sanathcoding.cameraxapp.CameraValues.hasPermission
+import com.sanathcoding.cameraxapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+
+    // If using cameraController
+    private lateinit var cameraController: LifecycleCameraController
+
+    // If using Camera Provide
+//    private val imageProvider: ImageCapture? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // request camera related permission
+        if (!hasPermission(baseContext)) activityResultLauncher.launch(REQUIRED_PERMISSION)
+//        else startCamera()
+    }
+
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permission ->
+        // Handle Permission Granted/Rejected
+        var permissionGranted = true
+        permission.entries.forEach {
+            if (it.key in REQUIRED_PERMISSION && !it.value) permissionGranted = false
+
+            if (!permissionGranted) Toast.makeText(
+                this,
+                "Permission request denied",
+                Toast.LENGTH_LONG
+            ).show()
+//            else startCamera()
+        }
     }
 }
